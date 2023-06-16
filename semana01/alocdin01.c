917d25826b8009f0b2d51f9c72b9bf0de0390c0b
gutormolina <augustomolina300@gmail.com>
2023-06-16 19:35:29 -0300

    Resolução do exercício 01 da lista de Alocação Dinâmica do Programação Descomplicada.

--- semana01/alocdin01.c
+++ semana01/alocdin01.c
@@ -2,5 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-    
+    int *v = (int *)malloc(5 * sizeof(int));
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &v[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", v[i]);
+    }
+    free(v);
 }
