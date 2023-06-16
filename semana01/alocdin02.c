20d7f1c06bb028c6e314a48b32d60a8ecf80a007
gutormolina <augustomolina300@gmail.com>
2023-06-16 20:10:39 -0300

    Correção do exercício 2.

--- semana01/alocdin02.c
+++ semana01/alocdin02.c
@@ -3,19 +3,18 @@
 
 int main()
 {
-    int *n = (int *)malloc(sizeof(int));
+    int n;
     printf("Insira o tamanho do vetor a ser lido: ");
-    scanf("%d", n);
-    int *v = (int *)malloc(*n * sizeof(int));
-    for (int i = 0; i < *n; i++)
+    scanf("%d", &n);
+    int *v = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
     {
         printf("Insira o valor para a posição %d do vetor: ", i);
         scanf("%d", &v[i]);
     }
-    for (int i = 0; i < *n; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d ", v[i]);
     }
-    free(n);
     free(v);
 }
