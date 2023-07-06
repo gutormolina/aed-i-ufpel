58462f41ed3a9add4ca992c36b0b95f4297f63f0
gutormolina <augustomolina300@gmail.com>
2023-07-06 19:25:22 -0300

    Resolução do exercício 1480 do Leet Code

---
+++ LC1480/src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* runningSum(int* nums, int numsSize, int* returnSize);
+
+int main()
+{
+    int tam = 10;
+    int* vetor = (int *)malloc(tam * sizeof(int));
+    int aux = 0;
+    vetor[0] = 1;
+    for (int i = 1; i < tam; i++)
+    {
+        vetor[i] = vetor[i - 1] + 1;
+    }
+    int* retorno = (int *)malloc(sizeof(int));
+    vetor = runningSum(vetor, tam, retorno);
+    for (int i = 0; i < tam; i++)
+    {
+        printf("%d ", vetor[i]);
+    }
+    free(vetor);
+    free(retorno);
+}
+
+int* runningSum(int* nums, int numsSize, int* returnSize){
+    int result = 0;
+    returnSize = numsSize;
+
+    for (int i = 0; i < numsSize; i++) 
+    {
+        result += nums[i];
+        nums[i] = result;
+    }
+    return nums;
+}
